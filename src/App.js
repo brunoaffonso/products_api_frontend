0c1c86b2ebb7900c5632b7d5312b5dcfafecba2f
brunoaffonso <brunoaffonso@gmail.com>
2022-07-29 22:50:52 -0300

    Authorization implementation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import Topbar from './components/Topbar/Topbar';
 import Content from './components/Content/Content';
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import PrivateRoute from './utils/PrivateRoute';
+// import PrivateRoute from './utils/PrivateRoute';
 import Footer from './components/Footer/Footer';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Home from './components/Home/Home';
+// import Login from './components/Login/Login';
+// import Register from './components/Register/Register';
+// import Home from './components/Home/Home';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <AuthProvider>
           <Topbar></Topbar>
+          <Content />
           <Routes>
-            <PrivateRoute component={Content} path="/protected" exact />
+            {/* <PrivateRoute component={Content} path="/protected" exact />
             <Route component={Login} path="/login" />
             <Route component={Register} path="/register" />
-            <Route component={Home} path="/" />
+            <Route component={Home} path="/" /> */}
+            <Route component={Content} path="/" />
           </Routes>
+          <Footer></Footer>
         </AuthProvider>
-        <Footer></Footer>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
